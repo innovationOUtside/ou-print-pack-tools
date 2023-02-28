9897a6bb7bf3d4a5ff1b1e1742b9d375e9116261
Tony Hirst <tony.hirst@gmail.com>
2023-02-28 15:17:10 +0000

    Make a package; add cli

--- README.md
+++ README.md
@@ -7,4 +7,15 @@ Support currently exists for:
 - generating OU branded PDF documents (one per notebook directory);
 - generating epub books (one per notebook directory).
 
-The intention is that the package can also be used as part of a Github Action automation script to generate print pack assets directly from content committed to a Github repository.
+The intention is that the package can also be used as part of a GitHub Action automation script to generate print pack assets directly from content committed to a GitHub repository.
+
+```text
+Usage: ou_nb_print_pack [OPTIONS]
+
+  Generate print materials from Jupyter notebooks.
+
+Options:
+  -n, --nbdir PATH   Path to weekly content folders [content]
+  -o, --outdir PATH  Path to output dir [print_pack]
+  --help             Show this message and exit.
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="ou_print_pack_tools",
+    packages=["ou_print_pack_tools"],
+    version='0.0.1',
+    author="Tony Hirst",
+    author_email="tony.hirst@gmail.com",
+    description="Generate print packs from Jupyter notebooks.",
+    long_description='''
+    Generate OU branded print packs from Jupyter notebooks.
+    ''',
+    long_description_content_type="text/markdown",
+    install_requires=["click",
+                      "ipython",
+                      "nbconvert",
+                      "nbformat",
+                      "pymupdf"
+                      ],
+    include_package_data=True,
+    package_data={'': ['resources/*']},
+    entry_points='''
+        [console_scripts]
+        ou_nb_print_pack=ou_print_pack_tools.print_publication:nb_to_print_pack
+    '''
+)
